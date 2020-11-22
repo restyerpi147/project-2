b128f87063eda912ec70b7c337b40d0a22e48d98
restyerpi147 <71168506+restyerpi147@users.noreply.github.com>
2020-11-22 20:39:45 +0700

    E-Commerce
    Membuat E-Commerce

---
+++ system/routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('about', function () {
+    return view('about');
+});
+
+Route::get('/ti', function () {
+    return view('ti');
+});
+
+Route::get('/menu' , function () {
+    return view('menu');
+});
+
+Route::get('/login' , function () {
+    return view('login');
+});
+
+
